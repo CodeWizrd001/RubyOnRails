c56e3cd11a18fd248885bff28564ba8bf26b3205
DragonPyli <codewizard001@gmail.com>
2021-11-07 18:38:45 +0530

    Added new controller
        Added test controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/test' , to: 'test#index'
+  get 'test/index'
+  get '/new' , to: 'new#index'
+  get 'new/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
